6b7942816fcc8e03143e2c8234d822faaf3ea939
seemoredebug <58932479+seemoredebug@users.noreply.github.com>
2022-11-21 22:25:49 +0800

    修改了数据库

--- Demo/src/main/java/com/example/demo/controller/StudentController.java
+++ Demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -6,7 +6,7 @@ import com.example.demo.entity.PageResult;
 import com.example.demo.entity.QueryPageBean;
 import com.example.demo.entity.Result;
 import com.example.demo.service.StudentService;
-import jdk.nashorn.internal.ir.annotations.Reference;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
